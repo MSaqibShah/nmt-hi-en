bafae7cafe26e564f55bada4d03e9a0a40900f22
S SHAH <msaqibshah001@gmail.com>
2023-12-21 20:07:59 +0530

    fix

--- main.py
+++ main.py
@@ -13,43 +13,19 @@ def api_test():
     return jsonify({'message': 'NMT Hindi To English is working'})
 
 
-@app.route('/translate', methods=['POST'])  
-def translate():
-    data = request.get_json(force=True)
-    if not data:
-        return jsonify({'message': 'No input data provided'}), 400
-    
-    text = data['text']
-
-    if not text:
-        return jsonify({'message': 'No text provided'}), 400  
-
-    if type(text) != list:
-        return jsonify({'message': 'Text should be a list'}), 400
-    
-    if len(text) > 1000:
-        return jsonify({'message': 'Text too long'}), 400
-
-    response = []
-    for line in text:
-        result = nmt_model.translate(line, source_lang="hi", target_lang="en")
-        response.append(result)
-
-    with open('output.txt', 'w',encoding='utf-8') as f:
-        for item in response:
-            item = " ".join(item)
-            f.write("%s\n" % item)
-
-    return jsonify(response), 200
-
-@app.route('/translate_file', methods=['GET'])
+@app.route('/translate', methods=['GET'])
 def translate_file():
     with open('input.txt', 'r',encoding='utf-8') as f:
         text = f.readlines()
 
+
+
     response = []
     for line in text:
-        result = nmt_model.translate(line, source_lang="hi", target_lang="en")
+        line = line.replace('\n','')
+        line = line.replace('\r','')
+        temp = f"{line}"
+        result = nmt_model.translate(temp, source_lang="hi", target_lang="en")
         response.append(result)
         
     with open('output.txt', 'w',encoding='utf-8') as f:
